2d04e35383832db235e7624d9e921ab14777af02
john <buzmakoff2005@mail.ru>
2020-06-27 22:12:39 +0300

    manycommit

--- pink/settings.py
+++ pink/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '@_7ez4e!pstah$tpp)z56z$ci5xew7
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
